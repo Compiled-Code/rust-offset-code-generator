44db2e8c3378de1d643140b11b3b7520aeff12a4
Compiled-Code <75095310+Compiled-Code@users.noreply.github.com>
2021-07-07 22:38:48 -0400

    Create generator.lua

---
+++ generator.lua
@@ -0,0 +1,33 @@
+local function to_snake_case(s)
+  s = s:gsub('(%S)(%u)', '%1_%2'):lower()
+
+  return s
+end
+
+local assemblies = mono_enumAssemblies();
+
+for i,v in pairs(assemblies) do
+    local image = mono_getImageFromAssembly(v);
+
+    local image_name = mono_image_get_name(image);
+
+    if (image_name == "Assembly-CSharp.dll") then
+       local classes = mono_image_enumClasses(image);
+
+       for i,v in pairs(classes) do
+           if (v.classname == "BasePlayer") then
+              local current_class = mono_findClass(v.namespace, v.classname);
+
+              local current_class_fields = mono_class_enumFields(current_class);
+
+              printf("namespace engine::offsets::" .. v.namespace .. to_snake_case(v.classname) .. "\n{");
+
+              for i2,v2 in pairs(current_class_fields) do
+                  print("	constexpr auto", to_snake_case(v2.name) .. " = 0x" .. string.format("%X", v2.offset) .. ";");
+              end
+
+              print("}");
+           end
+       end
+    end
+end
